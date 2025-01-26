022866b336059a560da0f4053960e7e9391e0879
dream-well <dreamwell506@outlook.com>
2025-01-25 19:58:20 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 source ~/.bashrc
 nvm install 18
 npm install -g pm2
+pip install fastapi uvicorn
 chmod +x ./test.sh
 ./test.sh
