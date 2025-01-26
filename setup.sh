4e273b644bfeeb31d97f6082ef3a9e617705d694
dream-well <dreamwell506@outlook.com>
2025-01-25 19:56:54 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 apt update
 apt install hashcat -y
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.bashrc
+nvm install 18
+npm install -g pm2
 chmod +x ./test.sh
 ./test.sh
